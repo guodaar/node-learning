94b2c4e9d9b281005b13cd15c5583c6e197e9e91
Guoda Arlauskaite <guoda.arlauskaite@gmail.com>
2022-12-06 18:42:33 +0200

    casual

--- 1 paskaita/index.js
+++ 1 paskaita/index.js
@@ -1,9 +1,14 @@
 const casual = require("casual");
 
-console.log(casual.city);
-
 const randomNumber = Math.floor(Math.random() * (10 - 1 + 1) + 1);
 casual.integer((from = 0), (to = 10));
 
-console.log(`${casual.city}, ${casual.integer((from = 0), (to = 10))}`);
+console.log(
+  `City: ${
+    casual.city
+  }, random number (old method): ${randomNumber}, random number (new method): ${casual.integer(
+    (from = 0),
+    (to = 10)
+  )}`
+);
 console.log(`${casual.name_prefix} ${casual.first_name} ${casual.last_name}`);
